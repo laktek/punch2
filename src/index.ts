a3e56fa8189e7bf255f9eeaa03c2274d6a329200
Lakshan Perera <lakshan@laktek.com>
2023-10-19 17:05:19 +1100

    basic CLI

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { Command } from "npm:commander";
+const program = new Command();
+
+program
+  .name("punch")
+  .description("A fast and simple static site builder")
+  .version("0.0.1");
+
+program.command("new")
+  .description("create a new site")
+  .action(() => {
+    console.log("not implemented");
+  });
+
+program.command("dev")
+  .description("run the site in dev mode (detects and rebuilds changed pages)")
+  .action(() => {
+    console.log("not implemented");
+  });
+
+program.command("build")
+  .description("generate a static site suitable for hosting")
+  .action(() => {
+    console.log("not implemented");
+  });
+
+program.command("serve")
+  .description(
+    "serves a static site in production mode with server-side rendering",
+  )
+  .action(() => {
+    console.log("not implemented");
+  });
+
+program.command("publish")
+  .description(
+    "publish the site to punch.site",
+  )
+  .action(() => {
+    console.log("not implemented");
+  });
+
+program.command("import")
+  .description(
+    "import any existing website and converts it to a Punch compatible site",
+  )
+  .action(() => {
+    console.log("not implemented");
+  });
+
+program.parse();
