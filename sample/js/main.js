51297ad4cb0bc3bb6388834d1de9aa6d38333e39
Lakshan Perera <lakshan@laktek.com>
2024-01-27 07:53:38 +1100

    removed sample from repo

--- sample/js/main.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import MyApp from "./app.jsx";
-
-// Render your React component instead
-const root = createRoot(document.getElementById("app"));
-root.render(MyApp());
