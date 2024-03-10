cd354c80476a2f1ffef78dd88e570345495d67ab
Lakshan Perera <lakshan@laktek.com>
2024-03-10 11:21:14 +1100

    create releases for multiple targets

---
+++ scripts/ci_release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# args
+# x86_64-unknown-linux-gnu, x86_64-pc-windows-msvc
+# x86_64-apple-darwin aarach64-apple-darwin
+
+set -e
+
+rm -rf ./bin
+deno run -A -c deno.json ./scripts/writeTemplate.ts
+deno compile -A -c deno.json --output bin/punch-${1} --reload --include ./src/lib/dev_watcher.ts --target ${1} ./src/index.ts
