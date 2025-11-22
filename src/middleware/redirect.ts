6a1114b4ef1633798785c0ae5915c53ea6b9da61
Lakshan Perera <lakshan@laktek.com>
2025-11-22 11:58:34 +1100

    fix: log redirects

--- src/middleware/redirect.ts
+++ src/middleware/redirect.ts
@@ -3,13 +3,16 @@ import { Context, NextFn } from "../lib/middleware.ts";
 export default async function (ctx: Context, next: NextFn): Promise<Response> {
   const { config, request } = ctx;
   const { pathname } = new URL(request.url);
+  const newCtx = { ...ctx };
+
+  // TODO: support wildcard path names
   const redirect = config.redirects && config.redirects[pathname];
   if (redirect) {
-    return Response.redirect(
+    newCtx.response = return Response.redirect(
       new URL(redirect.destination, request.url),
       redirect.permanent ? 301 : 302,
     );
-  } else {
-    return next()(ctx, next);
   }
+
+  return next()(newCtx, next);
 }
